0a0cf4a4405ada60f3bb94e25636dde11dcc2b82
roohullah-dev <roohanitech121@gmail.com>
2026-02-11 02:50:10 +0430

    Fix framer-motion import

--- src/components/Teams.jsx
+++ src/components/Teams.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Title from "./Title";
 import { teamData } from "../assets/assets";
-import { motion } from "motion/react";
+import { motion } from "framer-motion";
 
 function Teams() {
   return (
